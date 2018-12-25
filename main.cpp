43edb4c72729d90d72137b99f6b26008ee3f1b8b
Abdel <abdelkhayyam@gmail.com>
2018-12-25 12:13:36 -0400

    Add scientific functions

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
+
+double logbase(int num, int base)
+{ return log10(num) / log10(base); }
 
 
 int main()
@@ -12,30 +16,91 @@ int main()
 	{
 		while (true)
 		{
-			cout << "What operation would you like to perform?\n\n";
-			cout << "1.Addition\n2.Subtraction\n3.Multiplication\n4.Division\n5.Exit\n\n";
+			cout << "What type of operation would you like to perform?\n\n";
+			cout << "1.Basic\n2.Scientific\n3.Exit\n\n";
 			cin >> det;
 			system("cls");
-			if (det == 5) return 0;
-			if (det == 1 || det == 2 || det == 3 || det == 4) break;
+			if (det == 3) return 0;
+			if (det == 1 || det == 2) break;
 		}
-
-		cout << "Enter the first number: ";
-		cin >> num1;
-		cout << "Enter the second number: ";
-		cin >> num2;
-		cout << endl;
 
 		switch (det)
 		{
-			case 1: cout << num1 + num2; break;
-			case 2: cout << num1 - num2; break;
-			case 3: cout << num1 * num2; break;
-			case 4: cout << num1 / num2; break;
-		}
+			case 1:
+				while (true)
+				{
+					cout << "Choose an operation:\n\n";
+					cout << "1.Addition\n2.Subtraction\n3.Multiplication\n4.Division\n5.Back\n\n";
+					cin >> det;
+					system("cls");
+					if (det == 1 || det == 2 || det == 3 || det == 4 || det == 5) break;
+				}
 
-		cout << "\n\n";
-		system("pause");
-		system("cls");
+				if (det != 5)
+				{
+					cout << "Enter the first number: ";
+					cin >> num1;
+					cout << "Enter the second number: ";
+					cin >> num2;
+					cout << endl;
+
+					switch (det)
+					{
+					case 1: cout << num1 + num2; break;
+					case 2: cout << num1 - num2; break;
+					case 3: cout << num1 * num2; break;
+					case 4: cout << num1 / num2; break;
+					}
+
+					cout << "\n\n";
+					system("pause");
+					system("cls");
+				}
+				break;
+
+			case 2:
+				while (true)
+				{
+					cout << "Choose an operation\n\n";
+					cout << "1.Square\n2.Square root\n3.Exponential\n4.Logarithm\n5.Back\n\n";
+					cin >> det;
+					system("cls");
+					if (det == 1 || det == 2 || det == 3 || det == 4 || det == 5) break;
+				}
+
+				switch (det)
+				{
+					case 1:
+						cout << "Enter the number: ";
+						cin >> num1;
+						cout << "The square is " << num1 * num1 << "\n\n";
+						break;
+
+					case 2:
+						cout << "Enter the number: ";
+						cin >> num1;
+						cout << "The square root is " << sqrt(num1) << "\n\n";
+						break;
+
+					case 3:
+						cout << "Enter the base number: ";
+						cin >> num1;
+						cout << "Enter the exponent: ";
+						cin >> num2;
+						cout << "The result is " << pow(num1, num2) << "\n\n";
+						break;
+
+					case 4:
+						cout << "Enter the number: ";
+						cin >> num1;
+						cout << "Enter the logarithm base: ";
+						cin >> num2;
+						cout << "The result is " << logbase(num1, num2) << "\n\n";
+						break;
+				}
+
+				system("pause");
+				system("cls");
+		}
 	}
 }
